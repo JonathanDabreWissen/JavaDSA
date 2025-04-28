b4c67eab560bf07f1f7aa2f68de09bd3ee7e9f4e
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-04-28 15:46:23 +0530

    Add SubtractProductAndSum implementation

---
+++ SubtractProductAndSum.java
@@ -0,0 +1,25 @@
+public class SubtractProductAndSum {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.subtractProductAndSum(105));
+    }
+}
+
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int sum = 0;
+        int product = 1;
+
+        while(n!=0){
+            int d = n%10;
+            sum = sum + d;
+            product = product*d;
+
+            n = n/10;
+        }
+
+        return (product - sum);
+
+    }
+}
