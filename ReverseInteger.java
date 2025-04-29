ae815a2a4731c23d7c813d92e8b5882946ec5e34
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-04-29 15:48:28 +0530

    Enhance comments in ReverseInteger implementation for better clarity and understanding of the approach

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -51,3 +51,15 @@ class Solution {
 }
 
 //Approach:
+// int can store Integer.MAX_VALUE and negative Integer.MAX_VALUE + 1 i.e. Integer.MIN_VALUE
+// so if we reverse an integer it's value should be less Integer.MAX_VALUE. or +1
+// Simplicity we deal entirely with positive numbers so first step is to convert negative number if any to positive
+// In the above all inputs are integer so their both positive and negative storable except the Integer.MIN_VALUE, if that's the case we return 0;
+// To reverse an integer we extract the digit d using x%10 then x/10 until x==0.
+// Then we add the extracted digit this way reverseNumber = reverse*10 + d
+// Since we are going to multiply reverseNumber by 10, for this value to be able to storable in int, it should already be less than (Integer.MAX_VALUE/10) so that when it gets multiplied by 10 it will be less than (Integer.MAX_VALUE)
+// If reverseNumber is already  more than (Integer.MAX_VALUE/10) then we return 0
+// If reverseNumber is equal to (Integer.MAX_VALUE/10) then for positive number we check whether d>7, if yes then again it won't be storable in int so we return 0;
+// For negative number also same just that we have buffer of 1 so we check d>8.
+// if the reverse number passes all the ifs then for positive number we return directly the reverse number but for negative number we return -1*reverseNumber
+
