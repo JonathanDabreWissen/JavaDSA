479f8a07b4841c0512bd500fb24a4930cbff5c8d
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-05-08 11:51:36 +0530

    Fix mySqrt method to correctly calculate square root of 8 and improve getSquareRoot logic

--- SquareRoot2.java
+++ SquareRoot2.java
@@ -2,7 +2,7 @@ public class SquareRoot2 {
     public static void main(String[] args) {
         Solution s = new Solution();
        
-        System.out.println(s.mySqrt(28));
+        System.out.println(s.mySqrt(8));
     }
 }
 
@@ -12,46 +12,40 @@ class Solution {
             return x;
         }
 
-        int i = 1;
+        int i = 1; 
         int j = x;
-        return getSquareRoot(i, j, x);       
+        return getSquareRoot(i, j, x);  
     }
 
     public int getSquareRoot(int i, int j, int x){
-        if(i == j){
-            return i;
+        if(i > j){
+            return j;
         }
 
-        int m = j - ((i-j)/2); 
-        if(m == (x/m)){
+        int m = i + ((j-i)/2);  
+        if(m == (x/m)){ 
             return m;
         }
-        else if(m> (x/m)){
-            j = m;
-            return getSquareRoot(i, j, x);
+        else if(m> (x/m)){ 
+            j = m-1; 
+            return getSquareRoot(i, j, x); 
         }
         else{
-            i = m;
-            return getSquareRoot(i, j, x);
+            i = m+1; 
+            return getSquareRoot(i, j, x); 
         }
     }
 }
 
+//Approach
+// Use binary search type approach.
+// We have been given a number x, we have to find floor integer of its square root
+// What we do is use the binary search approach but without going over bound of integer limit
+// We take initial i and j range to be 1 to x
+// We first calculate m, to calculate m we use => m = i + (j-i)/2 this makes sure we are in integer limit.
+// Once m is there  we m*m == x ? but this can also go beyond integer instead we use m == x/m ?
+// if yes then we return m.
+// If not we further check if m> m/x if yes we reduce the j = m-1 and then return the function call again
+// else we increase i = m+1 and then return the function call again.
+// if (i>j) then we return j.
 
-// 100
-// 1 to 100
-// 50*50 > 100 => j = m;
-// 50*50 < 100 => i = m; 
-
-// m*m>x => m> (x/m)
-// m*m<x => m< (x/m) 
-
-// 1 - 101
-// 1 - 51
-// 1 - 26
-// 1 - 13
-// 7 - 13
-// 10 - 13
-// 10 - 12
-// 10 - 11
-// 10
